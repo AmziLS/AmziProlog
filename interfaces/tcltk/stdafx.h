574304ef3a614d76751d0c4815808b9f5f79bac2
Maël <42317414+maelh@users.noreply.github.com>
2021-05-07 21:24:30 +0200

    Update default project files like stdafx.h

--- interfaces/tcltk/stdafx.h
+++ interfaces/tcltk/stdafx.h
@@ -5,8 +5,12 @@
 
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
 #include <windows.h>
 
-// TODO: reference additional headers your program requires here
+
+
+// reference additional headers your program requires here
